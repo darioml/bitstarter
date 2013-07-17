a488b94e575abd3ddb0b96b49260f5b834aa6569
Dario Magliocchetti <dario_dml@hotmail.com>
2013-07-18 00:36:26 +0100

    Added new templating engine.. not much work done

--- web.js
+++ web.js
@@ -4,8 +4,16 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 app.use(express.static('public'));
 
+app.set('view engine', 'coffee')
+
+//consider moving to teacup?
+
+app.set('views', 'src/views')
+app.register('.coffee', require('coffeekup').adapters.express)
+
 app.get('/', function(request, response) {
-    response.send(fs.readFileSync('src/views/index.html').toString());
+	response.render('index.coffee')
+    //response.send(fs.readFileSync('src/views/index.html').toString());
 });
 
 var port = process.env.PORT || 5000;
